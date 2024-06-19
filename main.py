1e92d60fab07bcb408bb2a92254a0620359b3594
allison-0 <allisonulrich00@gmail.com>
2024-06-19 03:08:13 +0000

    msg

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
 def main():
-    """
-    ##################################################
-    # Comlete your code here
-    Use the same variables: celsius fahrenheit 
-    ##################################################
-    """
 
-    """
-    ########################################
+    # Comlete your code here
+    # Use the same variables: celsius fahrenheit 
+    celsius = float(input('Enter the temperature in celsius'))
+    fahrenheit = float(((9/5) * celsius)+32)
     # Do not delete the return statement
-    ########################################
-    """
+    print(fahrenheit)
+
     return celsius, fahrenheit
 
 
