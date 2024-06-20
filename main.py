02e220bf213443a3016073ae0fe0128b86e89ab9
allison-0 <allisonulrich00@gmail.com>
2024-06-20 11:58:43 -0700

    msg

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ def main():
     # Use the same variables: celsius fahrenheit 
     celsius = float(input('Enter the temperature in celsius'))
     fahrenheit = float(((9/5) * celsius)+32)
-    print(f'Fahrenheit: {fahrenheit:2.f}')
+    print(f'Fahrenheit: {fahrenheit:.2f}')
 
     # Do not delete the return statement
     return celsius, fahrenheit
